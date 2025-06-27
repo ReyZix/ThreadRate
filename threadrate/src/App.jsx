e45f02d85a23ced0181498b6307dc9b69ff8285f
Lorenzo Rey <96363551+ReyZix@users.noreply.github.com>
2025-06-27 01:12:55 -0400

    setup database as well as basic signup and login page that has working functionality

---
+++ threadrate/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './Signup';
+import Login from './Login'; 
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signup />} /> {/* ✅ Default route */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
