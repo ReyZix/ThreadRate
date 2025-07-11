284cf0da4da6b99715bb3b457ff223786581eba3
ShaneHiggins01 <117116127+ShaneHiggins01@users.noreply.github.com>
2025-07-11 15:36:07 -0400

    Add files via upload

---
+++ user.jsx
@@ -0,0 +1,16 @@
+class User {
+    username;
+    password;
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    get username() {
+        return this.username;
+    }
+
+    get password() {
+        return this.password;
+    }
+}
