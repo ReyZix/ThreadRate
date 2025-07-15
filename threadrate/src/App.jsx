efb651a88d5d1ac92050bc4424b64f450fb2bd31
Lorenzo Rey <96363551+ReyZix@users.noreply.github.com>
2025-07-15 12:29:50 -0400

    fixed app.jsx so it actually displays the home page

--- threadrate/src/App.jsx
+++ threadrate/src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login'; 
 import Upload from './Upload';
-import Home from './Home';
 
 function App() {
   return (
     <div className="min-h-screen bg-background text-foreground">
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} /> 
+          <Route path="/" element={<Home />} /> 
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/upload" element={<Upload />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
